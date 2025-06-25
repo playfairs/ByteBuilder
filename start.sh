e28a49289d6d3fc833c7430d3004f60b6552bf11
Playfair <74739795+playfairs@users.noreply.github.com>
2025-06-25 00:18:47 -0500

    added start.sh

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+mkdir -p build
+
+echo -e "${GREEN}Compiling file generator...${NC}"
+c++ -std=c++17 -O3 -o build/file_generator src/main.cpp
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Compilation successful! Running the program...${NC}\n"
+    
+    ./build/file_generator
+    
+    read -p "\nClean up build files? (y/n): " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        rm -rf build
+        echo "Build files cleaned up."
+    fi
+else
+    echo -e "${RED}Compilation failed. Please check the error messages above.${NC}"
+    exit 1
+fi
